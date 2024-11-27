0561e709025262241c3eff6bc578a75a0d1af154
Carl Pearson <cwpears@sandia.gov>
2024-11-27 08:57:39 -0700

    ci: enable linux build

--- scripts/make-generated.sh
+++ scripts/make-generated.sh
@@ -5,9 +5,9 @@ set -eou pipefail
 CMAKE_CURRENT_SOURCE_DIR="$1"
 CMAKE_CURRENT_BINARY_DIR="$2"
 
-version_string=$(xxd -v)
+version_string=$(xxd -v 2>&1)
 
-if [[ $version_string =~ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ]]; then
+if [[ "$version_string" =~ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ]]; then
     year="${BASH_REMATCH[1]}"
     month="${BASH_REMATCH[2]}"
     day="${BASH_REMATCH[3]}"
@@ -21,11 +21,12 @@ if [[ $version_string =~ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ]]; then
         xxd --name script_data -i ${CMAKE_CURRENT_SOURCE_DIR}/static/index.js | sed 's/^unsigned/static const/' >> ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
         exit 0
     else
-        xxd -i ${CMAKE_CURRENT_SOURCE_DIR}/static/index.html | sed 's/^unsigned/static const/' >  ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
-        xxd -i ${CMAKE_CURRENT_SOURCE_DIR}/static/index.css | sed 's/^unsigned/static const/' >> ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
-        xxd -i ${CMAKE_CURRENT_SOURCE_DIR}/static/index.js | sed 's/^unsigned/static const/' >> ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
+        cd ${CMAKE_CURRENT_SOURCE_DIR}/static
+        xxd -i index.html | sed 's/index_html/html_data/' | sed 's/^unsigned/static const/' >  ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
+        xxd -i index.css | sed 's/index_css/style_data/'| sed 's/^unsigned/static const/' >> ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
+        xxd -i index.js | sed 's/index_js/script_data/'| sed 's/^unsigned/static const/' >> ${CMAKE_CURRENT_BINARY_DIR}/generated.hpp
     fi
+else
+    echo "Invalid version format: $version_string"
+    exit 1
 fi
-
-echo "Invalid version format: $version_string"
-exit 1
